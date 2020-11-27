cd53cb2b7f88dc0e10a4e7762d7bfdaa106b9f36
nicolas sabogal <nicolassabogal85@gmail.com>
2020-11-27 03:00:06 -0500

    material good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -11,8 +14,10 @@ import { HeaderComponent } from './components/shared/header/header.component';
     HeaderComponent
   ],
   imports: [
+    MatSliderModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
