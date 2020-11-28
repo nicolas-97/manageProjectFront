d5087146d00d7bdbbd00348607c44274c5c55ab5
nicolas sabogal <nicolassabogal85@gmail.com>
2020-11-28 03:52:03 -0500

    Routing

--- src/app/components/login/register/register.component.ts
+++ src/app/components/login/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
 
-  constructor() { }
+  signUpForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private _builder: FormBuilder, private _authService:AuthService, private _router:Router) {
+    this.signUpForm = this._builder.group({
+      name: ['',Validators.required],
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.compose([Validators.minLength(8),Validators.required])],
+    });
   }
 
+  signUp(values){
+    console.log(values);
+  }
+
+  goBack(){
+    this._router.navigate(['login'])
+  }
 }
